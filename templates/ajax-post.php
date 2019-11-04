5a9044f2d243f2dd41141d57c5f994793b8e28d5
Vicente <Vicente@host.docker.internal>
2019-11-04 12:38:38 -0300

    Global $post

--- templates/ajax-post.php
+++ templates/ajax-post.php
@@ -1,8 +1,9 @@
 <?php
+    global $post;
+    
     if ( ! defined( 'ABSPATH' ) ) {
         exit;
     }
-    global $post;
 ?>
 <article itemtype="https://schema.org/CreativeWork" itemscope="itemscope" id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
     
